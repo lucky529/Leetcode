076d2733d6fcf4013deeef1180f0044fbfda0dff
lucky529 <1248327663@qq.com>
2019-07-23 00:48:07 +0800

    19.7.23

---
+++ mkdir/mkdir/test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		vector<string> list(n);
+		vector<bool> flag(n, true);
+		for (int i = 0; i < n; ++i) cin >> list[i];
+		sort(list.begin(), list.end());
+
+		for (int i = 0; i < list.size() - 1; ++i)
+		{
+			// 1、两串相同
+			// 2、前串是后串的子串，而且后串后一位是 '/'
+			if (list[i] == list[i + 1]) flag[i] = false;
+			else if (list[i].size() < list[i + 1].size() && \
+				list[i] == list[i + 1].substr(0, list[i].size()) && list[i + 1][list[i].size()] == '/')
+				flag[i] = false;
+		}
+
+		for (int i = 0; i < list.size(); ++i)
+		if (flag[i]) cout << "mkdir -p " << list[i] << endl;
+		cout << endl;
+	}
+	return 0;
+}
